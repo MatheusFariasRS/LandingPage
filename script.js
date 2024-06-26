4a0c5aafe52b30dd52a5ff53477185c45c1c3cd3
Matheus Farias <matheusaf.contato@gmail.com>
2024-06-25 23:55:56 -0300

    v 1.0

--- script.js
+++
@@ -1,5 +0,0 @@
-let padrao = document.getElementById('padrao')
-let plus = document.getElementById('plus')
-let ultra = document.getElementById('ultra')
-
-console.log(padrao, plus, ultra);
---
+++ script/script.js
@@ -0,0 +1,3 @@
+function contact(){
+    alert('Obrigado por entrar em contato!');
+}
